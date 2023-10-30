48aa59e5e7e8d6c3d2a8717df78fde457b892349
Vinícius Alves Borba <viniboal28@gmail.com>
2023-10-30 11:31:32 -0300

    test: add fetch recent questions E2E test

--- src/controllers/fetch-recent-questions.controller.ts
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -23,8 +23,8 @@ export class FetchRecentQuestionsController {
   @Get()
   async handle(@Query("page", queryValidationPipe) page: PageQueryParamSchema) {
     const questions = await this.prisma.question.findMany({
-      take: 1,
-      skip: (page - 1) * 1,
+      take: 20,
+      skip: (page - 1) * 20,
       orderBy: {
         createdAt: "desc",
       },
