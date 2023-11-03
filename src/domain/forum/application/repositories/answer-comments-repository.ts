29a3199a1aa594caab65d33954dff8fb1e1a3eb9
Vinícius Alves Borba <viniboal28@gmail.com>
2023-11-03 13:12:28 -0300

    feat: refactory of repositories

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,9 +1,9 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerComment } from '../../enterprise/entities/answer-comment'
 
-export interface AnswersCommentsRepository {
-  findById(id: string): Promise<AnswerComment | null>
-  findManyByAnswerId(answerId: string, params: PaginationParams): Promise<AnswerComment[]>
-  create(answerComment: AnswerComment): Promise<void>
-  delete(answerComment: AnswerComment): Promise<void>
+export abstract class AnswersCommentsRepository {
+  abstract findById(id: string): Promise<AnswerComment | null>
+  abstract findManyByAnswerId(answerId: string, params: PaginationParams): Promise<AnswerComment[]>
+  abstract create(answerComment: AnswerComment): Promise<void>
+  abstract delete(answerComment: AnswerComment): Promise<void>
 }
