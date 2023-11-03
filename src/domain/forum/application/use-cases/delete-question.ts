7b4942f99661086304a358274b3792ca83d2eca6
Vinícius Alves Borba <viniboal28@gmail.com>
2023-11-03 18:02:23 -0300

    test: add delete question e2e test

--- src/domain/forum/application/use-cases/delete-question.ts
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -2,6 +2,7 @@ import { Either, left, right } from "@/core/either";
 import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 import { NotAllowedError } from "./errors/not-allowed-error";
+import { Injectable } from "@nestjs/common";
 
 interface DeleteQuestionUseCaseRequest {
   authorId: string;
@@ -13,6 +14,7 @@ type DeleteQuestionUseCaseResponse = Either<
   null
 >;
 
+@Injectable()
 export class DeleteQuestionUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
 
