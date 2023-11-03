4487fc138a43d0d4fe0753451e02719d7db9acd8
Vinícius Alves Borba <viniboal28@gmail.com>
2023-11-03 18:15:54 -0300

    feat: add answer question controller

--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -1,8 +1,7 @@
 import { CurrentUser } from "@/infra/auth/current-user.decorator";
-import { JwtAuthGuard } from "@/infra/auth/jwt-auth.guard";
 import { UserPayload } from "@/infra/auth/jwt.strategy";
 import { ZodValidationPipe } from "@/infra/pipes/zod-validation-pipe";
-import { Controller, Post, UseGuards, Body, BadRequestException } from "@nestjs/common";
+import { Controller, Post, Body, BadRequestException } from "@nestjs/common";
 import { z } from "zod";
 import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
 
