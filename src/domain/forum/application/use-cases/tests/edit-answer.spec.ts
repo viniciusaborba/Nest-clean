9e82b40202b4b68559e1ec4b8c75af895b39cf04
Vinícius Alves Borba <viniboal28@gmail.com>
2023-11-14 12:32:20 -0300

    test: refactory of edit answer e2e test

--- src/domain/forum/application/use-cases/tests/edit-answer.spec.ts
+++ src/domain/forum/application/use-cases/tests/edit-answer.spec.ts
@@ -14,7 +14,9 @@ describe("Edit answer by id", () => {
   beforeEach(() => {
     inMemoryAnswerAttachmentsRepository =
       new InMemoryAnswerAttachmentsRepository();
-    inMemoryAnswersRepository = new InMemoryAnswersRepository();
+    inMemoryAnswersRepository = new InMemoryAnswersRepository(
+      inMemoryAnswerAttachmentsRepository
+    );
     sut = new EditAnswerUseCase(
       inMemoryAnswersRepository,
       inMemoryAnswerAttachmentsRepository
@@ -83,5 +85,48 @@ describe("Edit answer by id", () => {
 
     expect(result.isLeft()).toBe(true);
     expect(result.value).toBeInstanceOf(NotAllowedError);
+  });
+
+  it("should sync new and removed attachments while editing a answer", async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID("author-1"),
+      },
+      new UniqueEntityID("answer-1")
+    );
+
+    inMemoryAnswersRepository.create(newAnswer);
+
+    inMemoryAnswerAttachmentsRepository.items.push(
+      makeAnswerAttachment({
+        answerId: newAnswer.id,
+        attachmentId: new UniqueEntityID("1"),
+      }),
+
+      makeAnswerAttachment({
+        answerId: newAnswer.id,
+        attachmentId: new UniqueEntityID("2"),
+      })
+    );
+
+    const result = await sut.execute({
+      authorId: "author-1",
+      answerId: newAnswer.id.toString(),
+      content: "test content",
+      attachmentsIds: ["1", "3"],
+    });
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryAnswerAttachmentsRepository.items).toHaveLength(2)
+    expect(inMemoryAnswerAttachmentsRepository.items).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          attachmentId: new UniqueEntityID('1')
+        }),
+        expect.objectContaining({
+          attachmentId: new UniqueEntityID('3')
+        }),
+      ])
+    )
   });
 });
