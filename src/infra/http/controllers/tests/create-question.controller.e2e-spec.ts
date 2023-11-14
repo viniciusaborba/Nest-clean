66e5cbcff98e6fd2b6c5d6b274c7d2b8ab7aa03b
Vinícius Alves Borba <viniboal28@gmail.com>
2023-11-14 10:50:08 -0300

    test: refactory of create question e2e test

--- src/infra/http/controllers/tests/create-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/tests/create-question.controller.e2e-spec.ts
@@ -5,25 +5,28 @@ import { INestApplication } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { Test } from "@nestjs/testing";
 import request from "supertest";
+import { AttachmentFactory } from "test/factories/make-attachment";
 import { StudentFactory } from "test/factories/make-student";
 
 describe("Create question (E2E", () => {
   let app: INestApplication;
   let prisma: PrismaService;
+  let attachmentFactory: AttachmentFactory;
   let studentFactory: StudentFactory;
-  let jwt: JwtService
+  let jwt: JwtService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory],
+      providers: [StudentFactory, AttachmentFactory],
     }).compile();
 
     app = moduleRef.createNestApplication();
 
     prisma = moduleRef.get(PrismaService);
     studentFactory = moduleRef.get(StudentFactory);
-    jwt = moduleRef.get(JwtService)
+    attachmentFactory = moduleRef.get(AttachmentFactory);
+    jwt = moduleRef.get(JwtService);
 
     await app.init();
   });
@@ -31,24 +34,36 @@ describe("Create question (E2E", () => {
   test("[POST]/questions", async () => {
     const user = await studentFactory.makePrismaStudent();
 
-    const accessToken = jwt.sign({ sub: user.id.toString() })
+    const accessToken = jwt.sign({ sub: user.id.toString() });
+
+    const attachment1 = await attachmentFactory.makePrismaAttachment();
+    const attachment2 = await attachmentFactory.makePrismaAttachment();
 
     const response = await request(app.getHttpServer())
       .post("/questions")
-      .set('Authorization', `Bearer ${accessToken}`)
+      .set("Authorization", `Bearer ${accessToken}`)
       .send({
         title: "title content",
         content: "test content",
+        attachments: [attachment1.id.toString(), attachment2.id.toString()],
       });
 
     expect(response.statusCode).toBe(201);
 
     const questionOnDB = await prisma.question.findFirst({
       where: {
-        title: 'title content',
+        title: "title content",
       },
     });
 
     expect(questionOnDB).toBeTruthy();
+
+    const attachmentsOnDB = await prisma.attachment.findMany({
+      where: {
+        questionId: questionOnDB?.id,
+      }
+    })
+
+    expect(attachmentsOnDB).toHaveLength(2)
   });
 });
