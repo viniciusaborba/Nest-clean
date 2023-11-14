3392e19e5f029c766ed82a7bc1f0007632389f8c
Vinícius Alves Borba <viniboal28@gmail.com>
2023-11-14 11:17:55 -0300

    test: refactory of edit question e2e test

--- src/infra/http/controllers/tests/edit-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/tests/edit-question.controller.e2e-spec.ts
@@ -5,20 +5,29 @@ import { INestApplication } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { Test } from "@nestjs/testing";
 import request from "supertest";
+import { AttachmentFactory } from "test/factories/make-attachment";
 import { QuestionFactory } from "test/factories/make-question";
+import { QuestionAttachmentFactory } from "test/factories/make-question-attachment";
 import { StudentFactory } from "test/factories/make-student";
 
 describe("Edit question (E2E", () => {
   let app: INestApplication;
   let prisma: PrismaService;
   let studentFactory: StudentFactory;
-  let questionFactory: QuestionFactory
-  let jwt: JwtService
+  let attachmentFactory: AttachmentFactory;
+  let questionFactory: QuestionFactory;
+  let questionAttachmentFactory: QuestionAttachmentFactory;
+  let jwt: JwtService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory, QuestionFactory],
+      providers: [
+        StudentFactory,
+        QuestionFactory,
+        AttachmentFactory,
+        QuestionAttachmentFactory,
+      ],
     }).compile();
 
     app = moduleRef.createNestApplication();
@@ -26,7 +35,9 @@ describe("Edit question (E2E", () => {
     prisma = moduleRef.get(PrismaService);
     studentFactory = moduleRef.get(StudentFactory);
     questionFactory = moduleRef.get(QuestionFactory);
-    jwt = moduleRef.get(JwtService)
+    attachmentFactory = moduleRef.get(AttachmentFactory);
+    questionAttachmentFactory = moduleRef.get(QuestionAttachmentFactory);
+    jwt = moduleRef.get(JwtService);
 
     await app.init();
   });
@@ -34,31 +45,65 @@ describe("Edit question (E2E", () => {
   test("[PUT]/questions/:id", async () => {
     const user = await studentFactory.makePrismaStudent();
 
-    const accessToken = jwt.sign({ sub: user.id.toString() })
+    const accessToken = jwt.sign({ sub: user.id.toString() });
 
     const question = await questionFactory.makePrismaQuestion({
       authorId: user.id,
-    })
+    });
 
-    const questionId = question.id.toString()
+    const attachment1 = await attachmentFactory.makePrismaAttachment();
+    const attachment2 = await attachmentFactory.makePrismaAttachment();
+
+    await questionAttachmentFactory.makePrismaQuestionAttachment({
+      attachmentId: attachment1.id,
+      questionId: question.id,
+    });
+
+    await questionAttachmentFactory.makePrismaQuestionAttachment({
+      attachmentId: attachment2.id,
+      questionId: question.id,
+    });
+
+    const attachment3 = await attachmentFactory.makePrismaAttachment();
+
+    const questionId = question.id.toString();
 
     const response = await request(app.getHttpServer())
       .put(`/questions/${questionId}`)
-      .set('Authorization', `Bearer ${accessToken}`)
+      .set("Authorization", `Bearer ${accessToken}`)
       .send({
         title: "title content",
         content: "test content",
+        attachments: [attachment1.id.toString(), attachment3.id.toString()],
       });
 
     expect(response.statusCode).toBe(204);
 
     const questionOnDB = await prisma.question.findFirst({
       where: {
-        title: 'title content',
-        content: 'test content',
+        title: "title content",
+        content: "test content",
       },
     });
 
     expect(questionOnDB).toBeTruthy();
+
+    const attachmentsOnDB = await prisma.attachment.findMany({
+      where: {
+        questionId: questionOnDB?.id,
+      },
+    });
+
+    expect(attachmentsOnDB).toHaveLength(2);
+    expect(attachmentsOnDB).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: attachment1.id.toString(),
+        }),
+        expect.objectContaining({
+          id: attachment3.id.toString(),
+        }),
+      ])
+    );
   });
 });
