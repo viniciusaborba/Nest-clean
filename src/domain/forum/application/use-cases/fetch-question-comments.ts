e400bf081a7684021938e8fa6d5713bf77c86183
Vinícius Alves Borba <viniboal28@gmail.com>
2023-11-16 16:22:31 -0300

    feat: add CommentWithAuthor value object

--- src/domain/forum/application/use-cases/fetch-question-comments.ts
+++ src/domain/forum/application/use-cases/fetch-question-comments.ts
@@ -1,7 +1,7 @@
 import { Either, right } from '@/core/either'
-import { QuestionComment } from '../../enterprise/entities/question-comment'
 import { QuestionCommentsRepository } from '../repositories/question-comments-repository'
 import { Injectable } from '@nestjs/common'
+import { CommentWithAuthor } from '../../enterprise/entities/value-objects/comment-with-author'
 
 interface FetchQuestionCommentsUseCaseRequest {
   questionId: string
@@ -10,7 +10,7 @@ interface FetchQuestionCommentsUseCaseRequest {
 
 type FetchQuestionCommentsUseCaseResponse = Either<null, 
   {
-    questionComments: QuestionComment[]
+    comments: CommentWithAuthor[]
   }
 >
 
@@ -24,10 +24,10 @@ export class FetchQuestionCommentsUseCase {
     page,
     questionId,
   }: FetchQuestionCommentsUseCaseRequest): Promise<FetchQuestionCommentsUseCaseResponse> {
-    const questionComments = await this.questionCommentsRepository.findManyByQuestionId(questionId, { page })
+    const comments = await this.questionCommentsRepository.findManyByQuestionIdWithAuthor(questionId, { page })
 
     return right({
-      questionComments,
+      comments,
     })
   }
 }
