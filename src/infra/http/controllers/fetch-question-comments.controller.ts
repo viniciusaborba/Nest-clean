b96034de089aab8f835d8266a82ef59a11ace50d
Vinícius Alves Borba <viniboal28@gmail.com>
2023-11-16 16:53:04 -0300

    chore: implemented CommentWithAuthor presenter

--- src/infra/http/controllers/fetch-question-comments.controller.ts
+++ src/infra/http/controllers/fetch-question-comments.controller.ts
@@ -2,7 +2,7 @@ import { ZodValidationPipe } from "@/infra/pipes/zod-validation-pipe";
 import { BadRequestException, Controller, Get, Query, Param } from "@nestjs/common";
 import { z } from "zod";
 import { FetchQuestionCommentsUseCase } from "@/domain/forum/application/use-cases/fetch-question-comments";
-import { CommentPresenter } from "../presenters/comment-presenter";
+import { CommentWithAuthorPresenter } from "../presenters/comment-with-author-presenter";
 
 const pageQueryParamSchema = z
   .string()
@@ -33,10 +33,10 @@ export class FetchQuestionCommentsController {
       throw new BadRequestException()
     }
 
-    const questionComments = result.value.questionComments
+    const comments = result.value.comments
 
     return {
-      comments: questionComments.map(CommentPresenter.toHTTP)
+      comments: comments.map(CommentWithAuthorPresenter.toHTTP)
     };
   }
 }
