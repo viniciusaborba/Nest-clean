c72398928dad5f6e70893ddb87da6e5473ac58e5
Vinícius Alves Borba <viniboal28@gmail.com>
2023-11-17 11:08:07 -0300

    feat: : add findManyByAnswerIdWithAuthor method

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,9 +1,19 @@
-import { PaginationParams } from '@/core/repositories/pagination-params'
-import { AnswerComment } from '../../enterprise/entities/answer-comment'
+import { PaginationParams } from "@/core/repositories/pagination-params";
+import { AnswerComment } from "../../enterprise/entities/answer-comment";
+import { CommentWithAuthor } from "../../enterprise/entities/value-objects/comment-with-author";
 
 export abstract class AnswersCommentsRepository {
-  abstract findById(id: string): Promise<AnswerComment | null>
-  abstract findManyByAnswerId(answerId: string, params: PaginationParams): Promise<AnswerComment[]>
-  abstract create(answerComment: AnswerComment): Promise<void>
-  abstract delete(answerComment: AnswerComment): Promise<void>
+  abstract findById(id: string): Promise<AnswerComment | null>;
+  abstract findManyByAnswerId(
+    answerId: string,
+    params: PaginationParams
+  ): Promise<AnswerComment[]>;
+  
+  abstract findManyByAnswerIdWithAuthor(
+    answerId: string,
+    params: PaginationParams
+  ): Promise<CommentWithAuthor[]>;
+  
+  abstract create(answerComment: AnswerComment): Promise<void>;
+  abstract delete(answerComment: AnswerComment): Promise<void>;
 }
