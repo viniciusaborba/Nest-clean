481e9c8d1e1f976e9a3cb7ccd3f63935ee4baa0d
Vinícius Alves Borba <viniboal28@gmail.com>
2023-11-17 11:08:43 -0300

    feat: add CommentWithAuthor

--- src/domain/forum/application/use-cases/fetch-answer-comments.ts
+++ src/domain/forum/application/use-cases/fetch-answer-comments.ts
@@ -1,7 +1,7 @@
 import { Either, right } from "@/core/either";
-import { AnswerComment } from "../../enterprise/entities/answer-comment";
 import { AnswersCommentsRepository } from "../repositories/answer-comments-repository";
 import { Injectable } from "@nestjs/common";
+import { CommentWithAuthor } from "../../enterprise/entities/value-objects/comment-with-author";
 
 interface FetchAnswerCommentsUseCaseRequest {
   answerId: string;
@@ -11,7 +11,7 @@ interface FetchAnswerCommentsUseCaseRequest {
 type FetchAnswerCommentsUseCaseResponse = Either<
   null,
   {
-    answerComments: AnswerComment[];
+    comments: CommentWithAuthor[];
   }
 >;
 
@@ -23,13 +23,13 @@ export class FetchAnswerCommentsUseCase {
     page,
     answerId,
   }: FetchAnswerCommentsUseCaseRequest): Promise<FetchAnswerCommentsUseCaseResponse> {
-    const answerComments =
-      await this.answerCommentsRepository.findManyByAnswerId(answerId, {
+    const comments =
+      await this.answerCommentsRepository.findManyByAnswerIdWithAuthor(answerId, {
         page,
       });
 
     return right({
-      answerComments,
+      comments,
     });
   }
 }
