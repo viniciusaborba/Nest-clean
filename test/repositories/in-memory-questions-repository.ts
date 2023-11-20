6fd68671f9635d398283cef4c8e7423ecfdfbac5
Vinícius Alves Borba <viniboal28@gmail.com>
2023-11-20 15:54:37 -0300

    feat: implemented repository

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,14 +1,19 @@
 import { DomainEvents } from "@/core/events/domain-events";
 import { PaginationParams } from "@/core/repositories/pagination-params";
-import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
 import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
 import { Question } from "@/domain/forum/enterprise/entities/question";
+import { InMemoryAttachmentsRepository } from "./in-memory-attachments-repository";
+import { InMemoryStudentsRepository } from "./in-memory-students-repository";
+import { InMemoryQuestionAttachmentsRepository } from "./in-memory-question-attachments-repository";
+import { QuestionDetails } from "@/domain/forum/enterprise/entities/value-objects/question-details";
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = [];
 
   constructor(
-    private questionAttachmentRepository: QuestionAttachmentsRepository
+    private questionAttachmentRepository: InMemoryQuestionAttachmentsRepository,
+    private attachmentsRepository: InMemoryAttachmentsRepository,
+    private studentsRepository: InMemoryStudentsRepository,
   ) {}
 
   async findManyRecent({ page }: PaginationParams) {
@@ -47,6 +52,51 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     }
 
     return question;
+  }
+
+  async findDetailsBySlug(slug: string) {
+    const question = this.items.find((item) => item.slug.value === slug);
+
+    if (!question) {
+      return null;
+    }
+
+    const author = this.studentsRepository.items.find((student) => {
+      return student.id.equals(question.authorId)
+    })
+
+    if (!author) {
+      throw new Error(`Author with ID "${question.authorId.toString()}" does not exist`)
+    }
+
+    const questionAttachments = this.questionAttachmentRepository.items.filter((item) => {
+      return item.questionId.equals(question.id)
+    })
+
+    const attachments = questionAttachments.map((item) => {
+      const attachment = this.attachmentsRepository.items.find((attachment) => {
+        return attachment.id.equals(item.attachmentId)
+      })
+
+      if (!attachment) {
+        throw new Error(`Attachment with ID "${item.questionId.toString()}" does not exist`)
+      }
+
+      return attachment
+    })
+
+    return QuestionDetails.create({
+      questionId: question.id,
+      authorId: question.authorId,
+      author: author.name,
+      title: question.title,
+      slug: question.slug,
+      content: question.content,
+      bestAnswerId: question.bestAnswerId,
+      attachments,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    });
   }
 
   async create(question: Question) {
