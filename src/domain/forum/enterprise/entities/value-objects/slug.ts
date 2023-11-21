03a6ac9b6293106710e19919b945f2a8b86ffd92
Vinícius Alves Borba <viniboal28@gmail.com>
2023-11-21 12:18:23 -0300

    feat: add comment to slug entity

--- src/domain/forum/enterprise/entities/value-objects/slug.ts
+++ src/domain/forum/enterprise/entities/value-objects/slug.ts
@@ -1,24 +1,31 @@
 export class Slug {
   public value: string
-  
-  private constructor (value: string) {
+
+  private constructor(value: string) {
     this.value = value
   }
 
-  static create(slug: string) {
-    return new Slug(slug)
+  static create(value: string) {
+    return new Slug(value)
   }
 
-  static createFromText(text: string) {
+  /**
+   * Receives a string and normalize it as a slug.
+   *
+   * Example: "An example title" => "an-example-title"
+   *
+   * @param text {string}
+   */
+  static createFromText(text: string): Slug {
     const slugText = text
       .normalize('NFKD')
       .toLowerCase()
       .trim()
-      .replace(/\s+/g, '-') 
-      .replace(/[^\w-]+/g, '') 
-      .replace(/_/g, '-') 
-      .replace(/--+/g, '-') 
-      .replace(/-$/g, '') 
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '')
 
     return new Slug(slugText)
   }
