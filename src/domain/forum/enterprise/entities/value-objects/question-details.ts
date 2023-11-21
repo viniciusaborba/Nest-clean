64c607cfbd2e01cf5bf1011ab35c9e7aa6c14b90
Vinícius Alves Borba <viniboal28@gmail.com>
2023-11-21 12:17:52 -0300

    fix: fixed slug type

--- src/domain/forum/enterprise/entities/value-objects/question-details.ts
+++ src/domain/forum/enterprise/entities/value-objects/question-details.ts
@@ -1,13 +1,14 @@
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { ValueObject } from "@/core/entities/value-object";
 import { Attachment } from "../attachment";
+import { Slug } from "./slug";
 
 export interface QuestionDetailsProps {
   questionId: UniqueEntityID
   authorId: UniqueEntityID
   author: string
   title: string
-  slug: string
+  slug: Slug
   content: string
   attachments: Attachment[]
   bestAnswerId?: UniqueEntityID | null
