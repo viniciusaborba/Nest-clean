9521af26f439f83f46d41739987d4b242ff4ff0a
Vinícius Alves Borba <viniboal28@gmail.com>
2023-11-23 19:09:44 -0300

    chore: add redis to e2e setup

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -4,6 +4,10 @@ import { PrismaClient } from "@prisma/client";
 import { randomUUID } from "node:crypto";
 import { execSync } from "node:child_process";
 import { DomainEvents } from "@/core/events/domain-events";
+import { Redis } from "ioredis";
+import { envSchema } from "@/infra/env/env";
+
+const env = envSchema.parse(process.env)
 
 config({
   path: '.env', override: true,
@@ -14,13 +18,18 @@ config({
 })
 
 const prisma = new PrismaClient();
+const redis = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+})
 
 function generateUniqueDatabaseURL(schemaId: string) {
-  if (!process.env.DATABASE_URL) {
+  if (!env.DATABASE_URL) {
     throw new Error("Please provide a DATABASE_URL environment variable.");
   }
   
-  const url = new URL(process.env.DATABASE_URL)
+  const url = new URL(env.DATABASE_URL)
 
   url.searchParams.set('schema', schemaId)
 
@@ -35,6 +44,8 @@ beforeAll(async () => {
   process.env.DATABASE_URL = databaseUrl
 
   DomainEvents.shouldRun = false
+
+  await redis.flushdb()
 
   execSync('npx prisma migrate deploy')
 });
